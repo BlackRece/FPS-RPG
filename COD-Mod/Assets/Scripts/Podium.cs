44ca7e1f56cf7c9e2ba4d0a04cc8f14d36d11a84
Maurice Thompson-Hamilton <mauricemlth@hotmail.com>
2023-04-10 16:25:38 +0100

    animated intractable podiums
    all three podiums (weapon, resource and movement) have a spinning 3d icon and can only be interacted with when player is in contact with the podium's trigger object.
    
    next step:
    - killing enemies increases resource count

--- COD-Mod/Assets/Scripts/Podium.cs
+++ COD-Mod/Assets/Scripts/Podium.cs
@@ -10,9 +10,22 @@ public class Podium : MonoBehaviour, IInteractable
         Weapon,
     }
     
+    [SerializeField] private GameObject _IconGameObject;
+    [SerializeField, Range(0f, 100f)] private float _RotationSpeed = 50f;
     [SerializeField] private PodiumIDs _PodiumID;
     public PodiumIDs GetPodiumID() => _PodiumID;
     
+    private void Awake()
+    {
+        if (_IconGameObject == null)
+            throw new System.ArgumentNullException("ERROR: Icon GameObject is null!");
+    }
+
+    private void Update()
+    {
+        _IconGameObject.transform.Rotate(Vector3.up * Time.deltaTime * _RotationSpeed);
+    }
+
     public void OnInteract()
     {
         Debug.Log("Interacted with podium");
